fc0e0ed6db1aac44ff6c7967856f7dd43aefc723
john-laptop <iconium9000@gamil.com>
2020-03-08 01:23:52 -0700

    better foundation??

--- projects/circuit/client/ActComp.js
+++ projects/circuit/client/ActComp.js
@@ -12,108 +12,146 @@ module.exports = Circuit => {
   look.fun = {
     rep0: (acts,map,act) => {
       const link = ++map.length
-      map[link] = [
-        {tok:'newsave'},
-        {tok:'jumplink',link:act[1],catch:[
-          {tok:'clearsave'},
-          {tok:'jumpback'}
-        ]},
-        {tok:'addlist'},
-        {tok:'locksave'},
-        {tok:'jumplink',link:link},
-        {tok:'jumpback'}
-      ]
-      return list = [
-        {tok:'newlist'},
-        {tok:'jumplink',link:link},
-        {tok:'endlist'},
-        {tok:'jumpback'}
-      ]
+      map[link] = {
+        pre:{tok:'newsave'},
+        tok:'jumplink',link:act[1],
+        catch:{
+          pre:{tok:'clearsave'},
+          tok:'jumpback'
+        },
+        pst:{
+          pre:{
+            tok:'addlist',
+            pst:{tok:'locksave'},
+          },
+          tok:'jumplink',link:link,
+          catch:{tok:'jumpfail'},
+          pst:{tok:'jumpback'}
+        }
+      }
+      return {
+        pre:{tok:'newlist'},
+        tok:'jumplink',link:link,
+        catch:{tok:'jumpfail'},
+        pst:{
+          pre:{tok:'endlist'},
+          tok:'jumpback'
+        }
+      }
     },
     rep1: (acts,map,act) => {
       const link = ++map.length
-      map[link] = [
-        {tok:'newsave'},
-        {tok:'jumplink',link:act[1],catch:[
-          {tok:'clearsave'},
-          {tok:'jumpback'}
-        ]},
-        {tok:'addlist'},
-        {tok:'locksave'},
-        {tok:'jumplink',link:link},
-        {tok:'jumpback'}
-      ]
-      return list = [
-        {tok:'newlist'},
-        {tok:'jumplink',link:act[1]},
-        {tok:'addlist'},
-        {tok:'jumplink',link:link},
-        {tok:'endlist'},
-        {tok:'jumpback'}
-      ]
+      map[link] = {
+        pre:{tok:'newsave'},
+        tok:'jumplink',link:act[1],
+        catch:{tok:'clearsave'},
+        pst:{
+          pre:{
+            tok:'addlist',
+            pst:{tok:'locksave'},
+          },
+          tok:'jumplink',link:link,
+          catch:{tok:'jumpfail'}
+        }
+      }
+      return {
+        pre:{tok:'newlist'},
+        tok:'jumplink',link:act[1],
+        catch:{tok:'jumpfail'},
+        pst:{
+          pre:{tok:'addlist'},
+          tok:'jumplink',link:link,
+          catch:{tok:'jumpfail'},
+          pst:{tok:'endlist'}
+        }
+      }
+    },
+    lst: (acts,map,act) => {
+      let {length} = act, ret = {tok:'endlist'}
+      while (length > 1) {
+        ret.pre = {tok:'addlist'}
+        ret = {
+          tok:'jumplink',link:act[--length],
+          pst:ret,
+        }
+      }
+      ret.pre = {tok:'newlist'}
+      return ret
     },
 
     or: (acts,map,act) => {
-      const {length} = act
-      if (length == 1) return []
-      let ret = [
-        {tok:'jumplink',link:act[--length]},
-        {tok:'jumpback'}
-      ]
+      let {length} = act, ret = { tok:'jumpback' }
+      if (length == 1) return ret
+      ret = {
+        tok:'jumplink',link:act[--length],
+        catch:{tok:'jumpfail'}
+      }
       while (length > 1) {
-        ret = [{tok:'clearsave'}].concat(ret)
-        ret = [
-          {tok:'newsave'},
-          {tok:'jumplink',link:act[--length],catch:ret},
-          {tok:'jumpback'}
-        ]
+        if (ret.pre) ret.pre = {tok:'clearsave',pst:ret.pre}
+        else ret.pre = {tok:'clearsave'}
+        ret = {
+          pre:{tok:'newsave'},
+          tok:jumplink,link:act[--length],
+          catch:ret,
+          pst:{ tok:'locksave' }
+        }
       }
       return ret
     },
-    and: (acts,map,act) => {
-      const list = [], length = act.length-1
-      if (length < 1) return list
-      for (let i = 1; i < length; ++i) list.push(
-        {tok:'newsave'},
-        {tok:'jumplink',link:act[i]},
-        {tok:'clearsave'}
-      )
-      list.push({tok:'jumplink',link:act[length]})
-      return list
-    },
-    not: (acts,map,act) => { return [
-      {tok:'newsave'},
-      {tok:'jumplink',link:act[1],catch:[
-        {tok:'clearsave'},
-        {tok:'jumpback'}
-      ]},
-      {tok:'clearsave'},
-      {tok:'jumpfail'}
-    ]},
-    char: (acts,map,act) => { return [
-      { tok:'char', branch: {
-        '\\': [ {tok:'next'},{tok:'schar'},{tok:'next'} ]
-      }},
-      {tok:'rchar'},{tok:'next'}
-    ]},
-    cmp: (acts,map,act) => {
-
-    },
-
-
-    lst: (acts,map,act) => {
-      let list = [{tok:'newlist'}]
-      for (let i = 1; i < act.length; ++i) {
-        list.push({tok:'jumplink',link:act[i]},{tok:'addlist'})
+    and: (acts,map,act) =>  {
+      let {length} = act, ret = { tok:'jumpback' }
+      if (length == 1) return ret
+      ret = {
+        tok:'jumplink',link:act[--length],
+        catch:{tok:'jumpfail'}
       }
-      list.push({tok:'endlist'})
-      return list
-    }
+      while (length > 1) {
+        if (ret.pre) ret.pre = {tok:'clearsave',pst:ret.pre}
+        else ret.pre = {tok:'clearsave'}
+        ret = {
+          pre:{tok:'newsave'},
+          tok:'jumplink',link:act[--length],
+          catch:{tok:'jumpfail'},
+          pst:ret
+        }
+      }
+      return ret
+    },
+    not: (acts,map,act) => { return {
+      pre:{tok:'newsave'},
+      tok:'jumplink',link:act[1],
+      catch:{ tok:'clearsave' },
+      pst:{
+        pre:{tok:'clearsave'},
+        tok:'jumpfail'
+      }
+    }},
+    char: (acts,map,act) => { return {
+      tok:'char',
+      branch:{ '\\':{ pre:{tok:'next'}, tok:'schar', pst:{tok:'next'} } },
+      catch:{ pre:{tok:'rchar'}, tok:'next' }
+    }},
+    cmp: (acts,map,act) => {
+      let str = act[1], {length} = str
+      let ret = {tok:'txt',txt:str}
+      while (length > 0) {
+        ret.pre = {tok:'next'}
+        ret = {
+          tok:'char',
+          branch:{ [str[--length]]:ret },
+          catch:{tok:'jumpfail'}
+        }
+      }
+      return ret
+    },
   }
 
+
+
   return function ActComp({act,start}) {
+    log(act)
     const map = {length:act.length}
-    for (let i = 1; i < act.length; ++i) map[i] = look(act,map,i)
+    for (let i = 1; i <= act.length; ++i) map[i] = look(act,map,i)
     log(map)
 
     return 'error'
