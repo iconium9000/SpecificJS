9113466d16108dca45f369491ccf7e7bec09032f
john-acer <iconium9000@gmail.com>
2020-03-09 13:18:51 -0700

    maybe??

--- projects/circuit/client/ActComp.js
+++ projects/circuit/client/ActComp.js
@@ -13,139 +13,216 @@ module.exports = Circuit => {
     rep0: (acts,map,act) => {
       const link = ++map.length
       map[link] = {
-        pre:{tok:'newsave'},
+        save:['new'],
         tok:'jumplink',link:act[1],
-        catch:{ tok:'clearsave' },
-        pst:{
-          pre:{
-            tok:'addlist',
-            pst:{tok:'locksave'},
-          },
+        err:{save:['clear'],tok:'jumpret'},
+        ret:{
+          save:['lock'],
+          ary:['add'],
           tok:'jumplink',link:link,
-          catch:{tok:'jumpfail'},
+          ret:{tok:'jumpret'},
+          err:{tok:'jumperr'}
         }
       }
       return {
-        pre:{tok:'newlist'},
+        ary:['new'],
         tok:'jumplink',link:link,
-        catch:{tok:'jumpfail'},
-        pst:{tok:'endlist'}
+        err:{tok:'jumperr'},
+        ret:{ary:['end'],tok:'jumpret'}
       }
     },
     rep1: (acts,map,act) => {
       const link = ++map.length
       map[link] = {
-        pre:{tok:'newsave'},
+        save:['new'],
         tok:'jumplink',link:act[1],
-        catch:{tok:'clearsave'},
-        pst:{
-          pre:{
-            tok:'addlist',
-            pst:{tok:'locksave'},
-          },
+        err:{save:['clear'],tok:'jumpret'},
+        ret:{
+          save:['lock'],
+          ary:['add'],
           tok:'jumplink',link:link,
-          catch:{tok:'jumpfail'}
+          err:{tok:'jumperr'},
+          ret:{tok:'jumpret'}
         }
       }
       return {
-        pre:{tok:'newlist'},
+        ary:['new'],
         tok:'jumplink',link:act[1],
-        catch:{tok:'jumpfail'},
-        pst:{
-          pre:{tok:'addlist'},
+        err:{tok:'jumperr'},
+        ret:{
+          ary:['add'],
           tok:'jumplink',link:link,
-          catch:{tok:'jumpfail'},
-          pst:{tok:'endlist'}
+          err:{tok:'jumperr'},
+          ret:{ary:['end'],tok:'jumpret'}
         }
       }
     },
     lst: (acts,map,act) => {
-      let {length} = act, ret = {tok:'endlist'}
+      let {length} = act, ret = {ary:['end'],tok:'jumpret'}
       while (length > 1) {
-        ret.pre = {tok:'addlist'}
+        ret.ary.splice(0,0,'add')
         ret = {
+          ary:[],
           tok:'jumplink',link:act[--length],
-          pst:ret,
+          err:{tok:'jumperr'},
+          ret:ret,
         }
       }
-      ret.pre = {tok:'newlist'}
+      ret.ary.splice(0,0,'new')
       return ret
     },
 
     or: (acts,map,act) => {
-      let {length} = act, ret = { tok:'jumpback' }
+      let {length} = act, ret = { tok:'jumpret' }
       if (length == 1) return ret
       ret = {
+        save:[],
         tok:'jumplink',link:act[--length],
-        catch:{tok:'jumpfail'}
+        err:{tok:'jumperr'},
+        ret:ret
       }
       while (length > 1) {
-        if (ret.pre) ret.pre = {tok:'clearsave',pst:ret.pre}
-        else ret.pre = {tok:'clearsave'}
+        ret.save.splice(0,0,'clear')
         ret = {
-          pre:{tok:'newsave'},
+          save:['new'],
           tok:'jumplink',link:act[--length],
-          catch:ret,
-          pst:{ tok:'locksave' }
+          err:ret,
+          ret:{ save:['lock'], tok:'jumpret' }
         }
       }
       return ret
     },
     and: (acts,map,act) =>  {
-      let {length} = act, ret = { tok:'jumpback' }
+      let {length} = act, ret = { tok:'jumpret' }
       if (length == 1) return ret
       ret = {
+        save:[],
         tok:'jumplink',link:act[--length],
-        catch:{tok:'jumpfail'}
+        err:{tok:'jumperr'},
+        ret:ret
       }
       while (length > 1) {
-        if (ret.pre) ret.pre = {tok:'clearsave',pst:ret.pre}
-        else ret.pre = {tok:'clearsave'}
+        ret.save.splice(0,0,'clear')
         ret = {
-          pre:{tok:'newsave'},
+          save:['new'],
           tok:'jumplink',link:act[--length],
-          catch:{tok:'jumpfail'},
-          pst:ret
+          err:{save:['clear'],tok:'jumperr'},
+          ret:ret
         }
       }
       return ret
     },
     not: (acts,map,act) => { return {
-      pre:{tok:'newsave'},
+      save:['new'],
       tok:'jumplink',link:act[1],
-      catch:{ tok:'clearsave' },
-      pst:{
-        pre:{tok:'clearsave'},
-        tok:'jumpfail'
-      }
+      err:{ save:['clear'], tok:'jumpret' },
+      ret:{ save:['clear'], tok:'jumperr' }
     }},
     char: (acts,map,act) => { return {
       tok:'char',
-      branch:{ '\\':{ pre:{tok:'next'}, tok:'schar', pst:{tok:'next'} } },
-      catch:{ pre:{tok:'rchar'}, tok:'next' }
+      branch:{'\\':{
+        save:['next'],
+        tok:'char',
+        char:{
+          save:['next'],
+          ary:['schar'],
+          tok:'jumpret'
+        }
+      }},
+      char:{
+        ary:['char'],
+        save:['next'],
+        tok:'jumpret'
+      }
     }},
     cmp: (acts,map,act) => {
       let str = act[1], {length} = str
-      let ret = {tok:'txt',txt:str}
+      let ret = {
+        save:[],
+        ary:['txt',str],
+        tok:'jumpret'
+      }
       while (length > 0) {
-        ret.pre = {tok:'next'}
+        ret.save.splice(0,0,'next')
         ret = {
+          save:[],
           tok:'char',
           branch:{ [str[--length]]:ret },
-          catch:{tok:'jumpfail'}
+          char:{tok:'jumperr'}
         }
       }
       return ret
     },
   }
 
+  let stack = []
+  function flatten(acts,act,save,ary,ret,err) {
+    act.flag = true
+    let fun = flatten.fun[act.tok]
+    while (typeof fun == 'string') fun = flatten.fun[fun]
+    if (typeof fun != 'function') {
+      throw act
+    }
+    if (!act.save) act.save = []
+    if (!act.ary) act.ary = []
 
+    log('save',save,act.save)
+    log('ary',ary,act.ary)
+    save = save.concat(act.save)
+    ary = ary.concat(act.ary)
+    fun = fun(acts,act,save,ary,ret,err)
+
+    act.flag = false
+    return fun
+  }
+  flatten.fun = {
+    jumplink: (acts,act,save,ary,ret,err) => {
+      const tret = ret
+      ret = flatten(acts,act.ret,[],[],ret,err)
+      err = flatten(acts,act.err,[],[],tret,err)
+
+      let {link} = act
+      act = acts[link]
+      if (act.flag) return {
+        tok:'jumplink',link:link,
+        save:save,ary:ary,
+        ret:ret,err:err
+      }
+      else return flatten(acts,act,save,ary,ret,err)
+    },
+    char: (acts,act,save,ary,ret,err) => {
+      const ans = {
+        tok:'char',
+        branch:{},
+        char:flatten(acts,act.char,save,ary,ret,err)
+      }
+      for (const c in act.branch) {
+        ans.branch[c] = flatten(acts,act.branch[c],save,ary,ret,err)
+      }
+      return ans
+    },
+    jumpret: (acts,act,save,ary,ret,err) => {
+      if (ret) return flatten(acts,ret,save,ary)
+      else return { tok:'jumpret', save:save, ary:ary }
+    },
+    jumperr: (acts,act,save,ary,ret,err) => {
+      if (err) return flatten(acts,err,save,ary)
+      else return { tok:'jumperr', save:save, ary:ary }
+    }
+  }
 
   return function ActComp({act,start}) {
     log(act)
     const map = {length:act.length}
     for (let i = 1; i <= act.length; ++i) map[i] = look(act,map,i)
     log(map)
+
+    const flatmap = {}
+    for (let i = 1; i <= map.length; ++i) {
+      flatmap[i] = flatten(map,map[i],[],[])
+    }
+    log(flatmap)
+    log(stack)
 
     return 'error'
   }
