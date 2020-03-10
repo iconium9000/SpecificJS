96437aacec7dbfe717d41bac1f963e94287d9d55
john-acer <iconium9000@gmail.com>
2020-03-10 16:23:22 -0700

    master merge

--- projects/circuit/client/ActComp.js
+++ projects/circuit/client/ActComp.js
@@ -73,14 +73,8 @@ module.exports = Circuit => {
     },
 
     or: (acts,map,act) => {
-      let {length} = act, ret = { tok:'jumpret' }
-      if (length == 1) return ret
-      ret = {
-        save:[],
-        tok:'jumplink',link:act[--length],
-        err:{tok:'jumperr'},
-        ret:ret
-      }
+      let {length} = act, ret = { save:[], tok:'jumpret' }
+      if (length == 1) return ret; else ret.tok = 'jumperr'
       while (length > 1) {
         ret.save.splice(0,0,'clear')
         ret = {
@@ -92,21 +86,15 @@ module.exports = Circuit => {
       }
       return ret
     },
-    and: (acts,map,act) =>  {
-      let {length} = act, ret = { tok:'jumpret' }
+    and: (acts,map,act) => {
+      let {length} = act, ret = { save:[], tok:'jumpret' }
       if (length == 1) return ret
-      ret = {
-        save:[],
-        tok:'jumplink',link:act[--length],
-        err:{tok:'jumperr'},
-        ret:ret
-      }
       while (length > 1) {
         ret.save.splice(0,0,'clear')
         ret = {
           save:['new'],
           tok:'jumplink',link:act[--length],
-          err:{save:['clear'],tok:'jumperr'},
+          err:{ save:['clear'], tok:'jumperr' },
           ret:ret
         }
       }
@@ -123,29 +111,16 @@ module.exports = Circuit => {
       branch:{'\\':{
         save:['next'],
         tok:'char',
-        char:{
-          save:['next'],
-          ary:['schar'],
-          tok:'jumpret'
-        }
+        char:{ save:['next'], ary:['schar'], tok:'jumpret' }
       }},
-      char:{
-        ary:['char'],
-        save:['next'],
-        tok:'jumpret'
-      }
+      char:{ save:['next'], ary:['char'], tok:'jumpret' }
     }},
     cmp: (acts,map,act) => {
       let str = act[1], {length} = str
-      let ret = {
-        save:[],
-        ary:['txt',str],
-        tok:'jumpret'
-      }
+      let ret = { ary:['txt',str], tok:'jumpret' }
       while (length > 0) {
-        ret.save.splice(0,0,'next')
+        ret.save = ['next']
         ret = {
-          save:[],
           tok:'char',
           branch:{ [str[--length]]:ret },
           char:{tok:'jumperr'}
@@ -155,7 +130,68 @@ module.exports = Circuit => {
     },
   }
 
-  function flatten(acts,act,stack,ret,err) {
+  function matchary(a,b) {
+    return JSON.stringify(a) == JSON.stringify(b)
+  }
+  function popstack(stack,name) {
+    if (stack.parent) return popstack(stack.parent,name).concat(stack[name])
+    else return stack[name]
+  }
+  function stripstack(stack,keep,strip) {
+    if (stack) return {
+      parent: stripstack(stack.parent,keep,strip),
+      [keep]: stack[keep],
+      [strip]: []
+    }; else return false
+  }
+  function annihilate(save) {
+
+    let ret = [], stack = { parent:false, tok:'start', ret:[] }
+    for (let i = 0; i < save.length; ++i) switch (stack.tok) {
+      case 'lock': switch (save[i]) {
+        case 'new': {
+          stack.parent.ret.push(...stack.ret)
+          stack = stack.parent
+        } break;
+        case 'txt': stack.ret.push('txt',save[++i]); break;
+        default: ret.push(save[i]); break;
+      } break;
+      case 'new': switch (save[i]) {
+        case 'new': stack = { parent:stack, tok:'new', ret:[] }; break;
+        case 'lock': stack.parent.ret.push(...stack.ret)
+        case 'clear': stack = stack.parent; break;
+        case 'end': {
+          if (stack.ret.length == 0) {
+            stack = stack.parent
+            stack.ret.push('empty')
+          }
+          else {
+            stack.parent.ret.push('new',...stack.ret,stack.tok)
+            stack = stack.parent
+          }
+        } break;
+        case 'txt': stack.ret.push('txt',save[++i]); break;
+        default: stack.ret.push(save[i]); break;
+      } break;
+      default: switch (save[i]) {
+        case 'lock':
+        case 'new': stack = { parent:stack, tok:save[i], ret:[] }; break;
+        case 'txt': stack.ret.push('txt',save[++i]); break;
+        default: stack.ret.push(save[i]); break;
+      } break;
+    }
+    while (stack) {
+      switch (stack.tok) {
+        case 'lock':
+        case 'new': ret = [stack.tok].concat(stack.ret,ret); break;
+        case 'txt': stack.ret.push('txt',save[++i]); break;
+        default: ret = stack.ret.concat(ret); break;
+      }
+      stack = stack.parent
+    }
+    return ret
+  }
+  function flatten(acts,map,act,stack,ret,err) {
     act.flag = true
     let fun = flatten.fun[act.tok]
     while (typeof fun == 'string') fun = flatten.fun[fun]
@@ -168,10 +204,29 @@ module.exports = Circuit => {
     stack = stack || tstack
     for (const i in act.save) {
       let {parent,save,ary} = stack
-      stack = {
-        parent:parent,
-        save:save.concat(act.save[i]),
-        ary:ary
+      parent = parent || tstack
+      switch (act.save[i]) {
+        case 'new':stack = {
+          parent:stack,
+          save:['new'],
+          ary:[]
+        }; break;
+        case 'lock':stack = {
+          parent:parent.parent,
+          save:parent.save.concat(save,'lock'),
+          ary:parent.ary.concat(ary)
+        }; break;
+        case 'clear':stack = {
+          parent:parent.parent,
+          save:parent.save.concat(save,'clear'),
+          ary:parent.ary
+        }; break;
+        case 'next':stack = {
+          parent:parent,
+          save:save.concat('next'),
+          ary:ary
+        }; break;
+        default:
       }
     }
     if (act.ary) {
@@ -182,78 +237,19 @@ module.exports = Circuit => {
         ary:ary.concat(act.ary)
       }
     }
-    fun = fun(acts,act,stack,ret,err)
+    fun = fun(acts,map,act,stack,ret,err)
 
     act.flag = false
     return fun
   }
-  function popstack(stack,name) {
-    if (stack.parent) return popstack(stack.parent,name).concat(stack[name])
-    else return stack[name]
-  }
-  function annihilate(save) {
-    return save
-
-    let stack = { parent:false, ret:[] }
-
-    for (let i = 0; i < save.length; ++i) {
-      let {parent,ret} = stack
-      switch (save[i]) {
-        case 'new': {
-          stack = { parent:stack, ret:[] }
-          break;
-        }
-        case 'end': {
-          if (parent == false) stack.ret.push('end')
-          else if (stack.ret.length == 0) {
-            stack = parent
-            stack.ret.push('empty')
-          }
-          else {
-            stack = parent
-            stack.ret = stack.ret.concat('new',ret,'end')
-          }
-          break;
-        }
-        case 'lock': {
-          if (parent == false) stack.ret.push('lock')
-          else if (stack.ret.length == 0) stack = parent
-          else {
-            stack = stack.parent
-            stack.ret = stack.ret.concat(ret)
-          }
-          break;
-        }
-        case 'clear': {
-          if (parent == false) stack.ret.push('clear')
-          else stack = parent
-          break;
-        }
-        case 'txt': stack.ret.push(save[i],save[++i]); break;
-        case 'schar':
-        case 'char':
-        case 'add':
-        case 'next': stack.ret.push(save[i]); break;
-        default: break;
-      }
-    }
-
-    let ret = []
-    while (stack) {
-      if (stack.parent) ret = ['new'].concat(stack.ret,ret)
-      else ret = stack.ret.concat(ret)
-      stack = stack.parent
-    }
-    return ret
-  }
   flatten.fun = {
-    jumplink: (acts,act,stack,ret,err) => {
+    jumplink: (acts,map,act,stack,ret,err) => {
       const tret = ret
-      ret = flatten(acts,act.ret,false,ret,err)
-      err = flatten(acts,act.err,false,tret,err)
+      ret = flatten(acts,map,act.ret,false,ret,err)
+      err = flatten(acts,map,act.err,false,tret,err)
       let {link} = act; act = acts[link]
       if (act.flag) {
-        acts.flat[link] = true
+        map[link] = true
         return {
           tok:'jumplink',link:link,
           save:annihilate(popstack(stack,'save')),
@@ -262,56 +258,58 @@ module.exports = Circuit => {
           err:err
         }
       }
-      else return flatten(acts,act,stack,ret,err)
+      else return flatten(acts,map,act,stack,ret,err)
     },
-    char: (acts,act,stack,ret,err) => {
+    char: (acts,map,act,stack,ret,err) => {
       const ans = { tok:'char',branch:{} }
 
-      const {parent,save,ary} = stack
-      stack = { parent:parent, save:[], ary:ary }
-      ans.save = save
+      // log('char',stack)
 
-      const char = flatten(acts,act.char,stack,ret,err)
-      ans.char = char
+      let save = annihilate(popstack(stack,'save'))
+      if (save.includes('next') || save.includes('clear')) {
+        ans.save = save
+        stack = stripstack(stack,'ary','save')
+      }
+      else ans.save = []
 
-
-      // if (char.tok != 'char' || char.save.length > 0) ans.char = char
-      // else {
-      //   ans.char = char.char
-      //   for (const c in char.branch) {
-      //     ans.branch[c] = char.branch[c]
-      //   }
-      // }
+      const char = flatten(acts,map,act.char,stack,ret,err)
+      if (char.tok == 'char' && matchary(ans.save,char.save)) {
+        ans.char = char.char
+        for (const c in char.branch) {
+          ans.branch[c] = char.branch[c]
+        }
+      }
+      else ans.char = char
 
       for (const c in act.branch) {
-        ans.branch[c] = flatten(acts,act.branch[c],stack,ret,err)
+        ans.branch[c] = flatten(acts,map,act.branch[c],stack,ret,err)
       }
 
       return ans
     },
-    badchar: (acts,act,stack,ret,err) => {
+    badchar: (acts,map,act,stack,ret,err) => {
       const ans = { tok:'char',branch:{} }
 
       const {parent,save,ary} = stack
       stack = { parent:parent, save:[], ary:ary }
 
       ans.save = save
-      ans.char = flatten(acts,act.char,stack,ret,err)
+      ans.char = flatten(acts,map,act.char,stack,ret,err)
       for (const c in act.branch) {
-        ans.branch[c] = flatten(acts,act.branch[c],stack,ret,err)
+        ans.branch[c] = flatten(acts,map,act.branch[c],stack,ret,err)
       }
       return ans
     },
-    jumpret: (acts,act,stack,ret,err) => {
-      if (ret) return flatten(acts,ret,stack)
+    jumpret: (acts,map,act,stack,ret,err) => {
+      if (ret) return flatten(acts,map,ret,stack)
       else return {
         tok:'jumpret',
         save:annihilate(popstack(stack,'save')),
         ary:annihilate(popstack(stack,'ary'))
       }
     },
-    jumperr: (acts,act,stack,ret,err) => {
-      if (err) return flatten(acts,err,stack)
+    jumperr: (acts,map,act,stack,ret,err) => {
+      if (err) return flatten(acts,map,err,stack)
       return {
         tok:'jumperr',
         save:annihilate(popstack(stack,'save')),
@@ -322,21 +320,21 @@ module.exports = Circuit => {
 
   return function ActComp({act,start}) {
     log(act)
-    const map = {length:act.length,flat:{}}
+    const map = {length:act.length}
     for (let i = 1; i <= act.length; ++i) map[i] = look(act,map,i)
     log(map)
 
-    map.flat[start] = true
+    const flat = { [start]:true }
     while (true) {
       let flag = null
-      for (const i in map.flat) if (map.flat[i] == true) {
+      for (const i in flat) if (flat[i] == true) {
         flag = i
         break
       }
       if (flag == null) break
-      map.flat[flag] = flatten(map,map[flag],false)
+      flat[flag] = flatten(map,flat,map[flag],false)
     }
-    log(map.flat)
+    log(flat)
 
     return 'error'
   }
