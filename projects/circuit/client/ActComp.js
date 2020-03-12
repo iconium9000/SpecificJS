aa8d3b7efaedc05b830ac3b718431401ed5c61fb
john-acer <iconium9000@gmail.com>
2020-03-11 18:12:57 -0700

    Long walk/brainstorm

--- projects/circuit/client/ActComp.js
+++ projects/circuit/client/ActComp.js
@@ -1,98 +1,138 @@
 module.exports = Circuit => {
 
   function copy(...ary) { return Object.assign({},...ary) }
-  function looktok(acts,map,tok,state) {
+
+  function gotostrip(state,stripidx,fun) {
     state = copy(state)
-    switch (tok.tok) {
-      case 'char': {
-        let strip = state.strip[state.stripidx]
-        let ret = true, err = true
-        if (strip) {
-          const char = strip.char[tok.char]
-          if (char == false) ret = false
-          else if (char == true) err = false
-        }
-        else {
+    if (stripidx > state.stripidx) throw "strip can't jump forward"
+    else if (stripidx != undefined && stripidx != state.stripidx) do {
+      if (!state.strip) throw 'empty strip stack'
+      let {parent} = state
+
+      --state.stripidx
+    } while (stripidx < state.stripidx)
 
 
-          strip = { char:{}, link:{} }
-          state.strip[state.stripidx] = strip
-        }
-
-        throw 'strip'
-      }
-      case 'actid': {
-        if (tok.ret) state.ret = tok.ret
-        if (tok.err) state.err = tok.err
-        if (tok.fun) state.fun = state.fun.concat(tok.fun)
-        if (tok.stripidx >= 0) state.stripidx = tok.stripidx
-        return lookid(acts,map,tok.actid,state)
-      }
-      default: throw tok
-    }
+    return state
   }
-  function lookact(acts,map,act,state) {
-    let {length} = act
-    switch (act[0]) {
-      case 'lstadd': return looktok(acts,map,{
-        tok:'actid',actid:act[1],
-        ret:{ tok:'actid',actid:act[2],fun:['lstadd'] },
-        err:{ tok:'fun' }
-      },state)
-      case 'rep0': {
-        let link = ++acts.length
-        acts[link] = ['lstadd',act[1],link]
-        return looktok(acts,map,{
-          tok:'actid',actid:link,fun:['lstnew'],
-          ret:{ tok:'fun',fun:['lstend'] }
-        },state)
-      }
-      case 'lst': {
-        let tok = { tok:'fun',fun:['lstend'] }
-        while (length > 1) tok = {
-          tok:'actid',actid:act[--length],
-          fun:['lstadd'],ret:tok,
-        }
-        tok.fun.slice(0,0,'lstnew')
-        return looktok(acts,map,tok,state)
-      }
-      case 'or': {
-        let tok = false
-        while (length > 1) tok = {
-          tok:'actid',actid:act[--length],
-          err:tok,stripidx:state.stripidx
-        }
-        return looktok(acts,map,tok,state)
-      }
-      case 'cmp': {
-        let str = act[1], {length} = str, tok = { tok:'fun', fun:['txt',str] }
-        while (length > 0) tok = { tok:'char',char:--length,ret:tok }
-        return looktok(acts,map,tok,state)
-      }
-      default: error(...act); throw 'bad act'
-    }
+  function lookbranch(state,branch) {
+    log(state,branch)
+    throw 'lookbranch'
   }
-  function lookid(acts,map,actid,state) {
+  function lookid(state,actid) {
     if (state.stack.includes(actid)) {
-      log(state,actid)
-      throw 'linkid'
+      return lookbranch(state,{tok:'link',link:actid})
     }
     state = copy(state)
     state.stack = state.stack.concat(actid)
-    const act = acts[actid]
-    return lookact(acts,map,act,state)
+    return lookact(state,state.acts[actid])
+  }
+  function lookact(state,act) {
+    let {ret,err} = state; state = copy(state)
+    switch (act[0]) {
+      case 'lstbare': {
+        let {length} = act; ret = {tok:'ret',fun:['lstend'],ret:ret}
+        while (length > 1) ret = {
+          tok:'actid',actid:act[--length],fun:[],
+          ret:ret,err:err
+        }
+        ret.fun.splice(0,0,'lstnew')
+        return looktok(state,ret)
+      }
+      case 'lstadd': {
+        let [tok,actid,jump] = act
+        if (jump == undefined) state.ret = {tok:'ret',fun:['lstadd'],ret:ret}
+        else {
+          state.ret = {tok:'actid',fun:['lstadd'],actid:jump,ret:ret,err:err}
+          state.err = {tok:'ret',stripidx:state.stripidx,ret:ret}
+        }
+        return lookid(state,actid)
+      }
+      case 'or': {
+        let {length} = act
+        while (length > 1) err = {
+          tok:'actid',actid:act[--length],
+          ret:ret,err:err,
+          stripidx:state.stripidx
+        }
+        return looktok(state,err)
+      }
+      case 'lst': {
+        let {length} = act; ret = {tok:'ret',fun:['lstend'],ret:ret}
+        while (length > 1) {
+          ret.fun.splice(0,0,'lstadd')
+          ret = { tok:'actid',fun:[],actid:act[--length],ret:ret,err:err }
+        }
+        ret.fun.splice(0,0,'lstnew')
+        return looktok(state,ret)
+      }
+      case 'cmp': {
+        let str = act[1], {length} = str
+        ret = {tok:'ret',fun:['txt',str],ret:ret}
+        err = {tok:'err',stripidx:state.stripidx,err:err}
+        while (length > 0) ret = {tok:'char',char:str[--length],ret:ret,err:err}
+        return looktok(state,ret)
+      }
+      default: error(...act); throw 'lookact'
+    }
+  }
+  function looktok(state,tok) {
+    state = gotostrip(state,tok.stripidx,tok.fun || [])
+    state.err = tok.err
+    state.ret = tok.ret
+
+    switch (tok.tok) {
+      case 'ret': {
+        if (state.ret) return looktok(state,state.ret)
+        else throw 'ret'
+      }
+      case 'err': {
+        if (state.err) return looktok(state,state.err)
+        else throw 'err'
+      }
+      case 'char': {
+        const {stripidx,ret} = state
+        state.ret = {tok:'ret',fun:['char'],stripidx:stripidx+1,ret:ret}
+        return lookbranch(state,{tok:'char',char:tok.char})
+      }
+      case 'actid': return lookid(state,tok.actid)
+      default: error(tok); throw 'looktok'
+    }
   }
 
+
   return function ActComp({act,start}) {
+
+    let rep = {}
+    for (let i = 1; i < act.length; ++i) {
+      let [tok,actid] = act[i]
+      let rep0 = tok == 'rep0'
+      if (rep0 || tok == 'rep1') {
+        let repid = rep[actid]
+        if (!repid) {
+          rep[actid] = repid = ++act.length
+          act[repid] = ['lstadd',actid,repid]
+        }
+        if (rep0) act[i] = ['lstbare',repid]
+        else {
+          let addid = ++act.length
+          act[addid] = ['lstadd',actid]
+          act[i] = ['lstbare',addid,repid]
+        }
+      }
+    }
+    log(act)
     const map = {}
-    lookid(act,map,start,{
+    let ret = lookid({
+      acts:act,
+      map:map,
       stack:[],
-      strip:[],
-      fun:[],
+      strip:false,
       stripidx:0,
-      ret:{tok:'end'},
-      err:{tok:'err'}
-    })
+      ret:false,
+      err:false,
+    },start)
+    log(map,ret)
 
     return 'ActComp'
   }
